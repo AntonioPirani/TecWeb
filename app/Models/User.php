7346cfc9d89f5ff64eb88e9cd643c163590c2b0f
Luca Sabatini <s1099243@studenti.univpm.it>
2023-08-13 17:35:05 +0200

    Modifiche su file User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,9 +6,12 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+use Illuminate\Database\Eloquent\Model;
+
 class User extends Authenticatable
 {
     use Notifiable;
+    protected $table = 'utente';
 
     /**
      * The attributes that are mass assignable.
@@ -18,9 +21,16 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'surname',
+        'data_di_nascita',
         'email',
         'username',
         'password',
+    ];
+
+    protected $guarded = [
+        'id', // id (autoincrement?) dell'utente
+        'livello'   //livello utente specifica cliente, staff, admin
+
     ];
 
     /**
@@ -29,7 +39,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $hidden = [
-        'username',
+        'username', /*forse da togliere da hidden*/
         'password',
         'remember_token',
     ];
@@ -41,8 +51,12 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+         //aggiunta
+        'name','surname','username','password', 'email' => 'string',
+        'remember_token' => 'int',
+        'data_di_nascita' => 'date'
     ];
-    
+
     public function hasRole($role) {
         $role = (array)$role;
         return in_array($this->role, $role);
