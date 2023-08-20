40fe799779578dfefd0f733d550c34cc4283e0db
Luca Sabatini <s1099243@studenti.univpm.it>
2023-08-20 16:21:27 +0200

    Aggiunta bottoni area admin

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -7,6 +7,13 @@
     <h3>Area Amministratore</h3>
     <p>Benvenuto {{ Auth::user()->name }} {{ Auth::user()->surname }}</p>
     <p>Seleziona la funzione da attivare</p>
+
+    <!--bottoni -->
+    <ul>
+        <li><button><a href="{{ route('newproduct') }}" title="Inserisce nuovi prodotti">Inserisci</a></button></li>
+        <li><button><a href="{{ route('admin') }}" title="Inserisce nuovi prodotti"> Modifica</a></button></li>
+        <li><button><a href="{{ route('admin') }}" title="Cancella o prodotti">Cancella</a></li>
+    </ul>
 </div>
 @endsection
 
