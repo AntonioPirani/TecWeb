c5572e4481adcb74cbf86a978a1c98e6ae258a12
Luca Sabatini <s1099243@studenti.univpm.it>
2023-09-03 10:07:23 +0200

    Piccole modifiche (colore titolo faqs)

--- resources/views/who.blade.php
+++ resources/views/who.blade.php
@@ -3,33 +3,27 @@
 @section('title', 'Chi Siamo')
 
 @section('content')
-<div class="static">
-<head>
-  <style>
-    table {
-      width: 100%;
-    }
-    td {
-      vertical-align: top;
-      width: 50%;
-    }
-  </style>
-</head>
-<body>
+    <div class="static">
 
-<table>
-  <tr>
-    <td>
-      <h2>Chi siamo</h2>
-      <p>Siamo un team appassionato di esperti nel settore dell'automotive, determinati a rendere il tuo noleggio auto un'esperienza senza stress e piacevole. Con anni di esperienza nel campo, ci dedichiamo a fornire un servizio impeccabile, garantendo la massima comodità e soddisfazione in ogni singolo chilometro.</p>
-    </td>
-    <td>
-      <h2>La nostra missione</h2>
-      <p>La nostra missione è offrirti non solo una semplice auto a noleggio, ma un compagno di viaggio affidabile. Vogliamo rendere ogni viaggio un'avventura senza pensieri, offrendoti veicoli in condizioni ottimali e un servizio clienti sempre pronto ad assisterti.</p>
-    </td>
-  </tr>
-</table>
- <div><h2><center><a href="{{ route('auto') }}" title="Home">  Partiamo! </center></h2></div>
-</body>
+        <table>
+            <tr>
+                <td>
+                    <h2>Chi siamo</h2>
+                    <p>Siamo un team appassionato di esperti nel settore dell'automotive, determinati a rendere il tuo
+                        noleggio auto un'esperienza senza stress e piacevole. Con anni di esperienza nel campo, ci
+                        dedichiamo a fornire un servizio impeccabile, garantendo la massima comodità e soddisfazione in
+                        ogni singolo chilometro.</p>
+                </td>
+                <td>
+                    <h2>La nostra missione</h2>
+                    <p>La nostra missione è offrirti non solo una semplice auto a noleggio, ma un compagno di viaggio
+                        affidabile. Vogliamo rendere ogni viaggio un'avventura senza pensieri, offrendoti veicoli in
+                        condizioni ottimali e un servizio clienti sempre pronto ad assisterti.</p>
+                </td>
+            </tr>
+        </table>
+        <div style="text-align: center;"><h2>
+                <a href="{{ route('auto') }}" title="Home"> Partiamo! </a>
+            </h2></div>
 
 @endsection
