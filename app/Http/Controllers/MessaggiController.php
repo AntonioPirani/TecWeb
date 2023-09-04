872b8154bf91aa9e9da2033a7ab8cdb10ac18b44
Luca Sabatini <s1099243@studenti.univpm.it>
2023-09-04 11:27:04 +0200

    messaggi v1

---
+++ app/Http/Controllers/MessaggiController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MessaggiController extends Controller
+{
+    public function inboxAdmin(Request $request){
+//        $inbox=MessaggiController::where('')
+    }
+}
